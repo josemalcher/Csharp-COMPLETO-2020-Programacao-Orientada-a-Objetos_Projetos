beaabb00d0f6ef0255b74bf09a4d103e54770c8f
José Malcher Jr <contato@josemalcher.net>
2020-12-11 11:13:04 -0300

    Introdução a C# e .NET

---
+++ 1-Secao/ConsoleApp1-primeiro/ConsoleApp1-primeiro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1_primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
