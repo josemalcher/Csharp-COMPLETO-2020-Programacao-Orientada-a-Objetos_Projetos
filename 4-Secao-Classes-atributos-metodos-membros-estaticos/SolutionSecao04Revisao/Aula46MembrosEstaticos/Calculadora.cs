874d023e95c7584b1b2a07ed235a1b8660bf22af
Jose Malcher Jr <contato@josemalcher.net>
2022-04-26 09:33:55 -0300

    47 Membros estáticos - PARTE 2

---
+++ 4-Secao-Classes-atributos-metodos-membros-estaticos/SolutionSecao04Revisao/Aula46MembrosEstaticos/Calculadora.cs
@@ -0,0 +1,14 @@
+﻿namespace Aula46MembrosEstaticos;
+
+public class Calculadora
+{
+    public static double Pi = 3.14;
+    
+    public static double Circunferencia(double r) {
+        return 2.0 * Pi * r;
+    }
+    
+    public static double Volume(double r) {
+        return 4.0 / 3.0 * Pi * r * r * r;
+    }
+}
--- 4-Secao-Classes-atributos-metodos-membros-estaticos/SolutionSecao04Revisao/Aula46MembrosEstaticos/Program.cs
+++ 4-Secao-Classes-atributos-metodos-membros-estaticos/SolutionSecao04Revisao/Aula46MembrosEstaticos/Program.cs
@@ -5,25 +5,27 @@ namespace Aula46MembrosEstaticos // Note: actual namespace depends on the projec
 {
     internal class Program
     {
-        static double Pi = 3.14;
+        // static double Pi = 3.14;
         static void Main(string[] args)
         {
+
+            // Calculadora calculadora = new Calculadora();
             
             Console.Write("Entre o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double circ = Circunferencia(raio);
-            double volume = Volume(raio);
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
             Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
 
         }
-        static double Circunferencia(double r) {
+        /*static double Circunferencia(double r) {
             return 2.0 * Pi * r;
         }
         static double Volume(double r) {
             return 4.0 / 3.0 * Pi * r * r * r;
-        }
+        }*/
     }
 }
 
