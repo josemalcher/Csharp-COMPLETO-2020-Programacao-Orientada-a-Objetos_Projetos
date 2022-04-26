0f36169c4c41df8dc5a969206e9b985dc4e655df
Jose Malcher Jr <contato@josemalcher.net>
2022-04-26 10:13:02 -0300

    51 Construtores

---
+++ 5-Secao-Construtores-palavra-this-sobrecarga-encapsulamento/SolutionSecao05Revisao/SolutionSecao05Revisao/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace SolutionSecao05Revisao;
+
+public class Produto
+{
+    public string Nome;
+    public double Preco;
+    public int Quantidade;
+
+    public Produto(string nome, double preco, int quantidade)
+    {
+        Nome = nome;
+        Preco = preco;
+        Quantidade = quantidade;
+    }
+
+    public double ValorTotalEmEstoque() {
+        return Preco * Quantidade;
+    }
+    public void AdicionarProdutos(int quantidade) {
+        Quantidade += quantidade;
+    }
+    public void RemoverProdutos(int quantidade) {
+        Quantidade -= quantidade;
+    }
+    public override string ToString() {
+        return Nome
+               + ", $ "
+               + Preco.ToString("F2", CultureInfo.InvariantCulture)
+               + ", "
+               + Quantidade
+               + " unidades, Total: $ "
+               + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+    }
+}
