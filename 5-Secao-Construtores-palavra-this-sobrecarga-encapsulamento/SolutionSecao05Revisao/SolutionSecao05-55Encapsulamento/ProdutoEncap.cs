bca53fc39458a30d8262e12206044727b616e462
Jose Malcher Jr <contato@josemalcher.net>
2022-04-27 15:05:21 -0300

    57 Auto Properties

---
+++ 5-Secao-Construtores-palavra-this-sobrecarga-encapsulamento/SolutionSecao05Revisao/SolutionSecao05-55Encapsulamento/ProdutoEncap.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace SolutionSecao05Revisao;
+
+public class ProdutoEncap
+{
+    private string _nome;
+            public double Preco { get; private set; }
+            public double Quantidade { get; set; }
+            public ProdutoEncap() {
+            }
+            public ProdutoEncap(string nome, double preco, int quantidade) {
+                _nome = nome;
+                Preco = preco;
+                Quantidade = quantidade;
+            }
+            public string Nome {
+                get { return _nome; }
+                set {
+                    if (value != null && value.Length > 1) {
+                        _nome = value;
+                    }
+                }
+            }
+            public double ValorTotalEmEstoque {
+                get { return Preco * Quantidade; }
+            }
+            public void AdicionarProdutos(int quantidade) {
+                Quantidade += quantidade;
+            }
+            public void RemoverProdutos(int quantidade) {
+                Quantidade -= quantidade;
+            }
+            public override string ToString() {
+                return _nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque.ToString("F2", CultureInfo.InvariantCulture);
+            }
+}
--- 5-Secao-Construtores-palavra-this-sobrecarga-encapsulamento/SolutionSecao05Revisao/SolutionSecao05-55Encapsulamento/Program.cs
+++ 5-Secao-Construtores-palavra-this-sobrecarga-encapsulamento/SolutionSecao05Revisao/SolutionSecao05-55Encapsulamento/Program.cs
@@ -1,10 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Globalization;
-using System.Reflection.Metadata.Ecma335;
+
 using SolutionSecao05Revisao;
 
-Produto p = new Produto("TV", 500.00, 10);
+ProdutoEncap p = new ProdutoEncap("TV", 500.00, 10);
 
 p.Nome = "TV 4k";
 
