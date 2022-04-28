e797f60ab00ca083fae61377eb98d9311049871c
Jose Malcher Jr <contato@josemalcher.net>
2022-04-28 09:17:41 -0300

    66 Tipos referência e tipos valor

---
+++ 6-Comportamento-de-memoria-arrays-listas/SolutionSecao6Revisao/SolutionSecao6Revisao_66RefValor/Point.cs
@@ -0,0 +1,9 @@
+﻿namespace SolutionSecao6Revisao_66RefValor;
+
+public struct Point
+{
+    public double X, Y;
+    public override string ToString() {
+        return "(" + X + "," + Y + ")";
+    }
+}
---
+++ 6-Comportamento-de-memoria-arrays-listas/SolutionSecao6Revisao/SolutionSecao6Revisao_66RefValor/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SolutionSecao6Revisao_66RefValor;
+
+Point p;
+// Console.WriteLine(p); // erro: variável não atribuida
+p.X = 10;
+p.Y = 20;
+Console.WriteLine(p);
+
+p = new Point();
+Console.WriteLine(p);
