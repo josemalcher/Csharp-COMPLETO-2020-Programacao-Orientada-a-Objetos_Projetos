e972651b13b4099b8063e8aaee9fb93d20cc9ac0
Jose Malcher Jr <contato@josemalcher.net>
2022-05-02 12:05:49 -0300

    71 Exercício de fixação (vetores)

---
+++ 6-Comportamento-de-memoria-arrays-listas/SolutionSecao6Revisao/Exer1/Estudante.cs
@@ -0,0 +1,18 @@
+﻿namespace Exer1;
+
+public class Estudante
+{
+    public string Nome{get; set;}
+    public string Email { get; set; }
+
+    public Estudante(string nome, string email)
+    {
+        Nome = nome;
+        Email = email;
+    }
+
+    public override string ToString()
+    {
+        return Nome + ", " + Email;
+    }
+}
---
+++ 6-Comportamento-de-memoria-arrays-listas/SolutionSecao6Revisao/Exer1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Exer1;
+
+Estudante[] vect = new Estudante[10];
+Console.Write("Quantos quartos serão alugados? ");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Aluguel #{i}:");
+    Console.Write("Nome: ");
+    string nome = Console.ReadLine();
+    Console.Write("Email: ");
+    string email = Console.ReadLine();
+    Console.Write("Quarto: ");
+    int quarto = int.Parse(Console.ReadLine());
+    vect[quarto] = new Estudante(nome, email);
+}
+Console.WriteLine();
+Console.WriteLine("Quartos ocupados:");
+for (int i = 0; i < 10; i++)
+{
+    if (vect[i] != null)
+    {
+        Console.WriteLine(i + ": " + vect[i]);
+    }
+}
