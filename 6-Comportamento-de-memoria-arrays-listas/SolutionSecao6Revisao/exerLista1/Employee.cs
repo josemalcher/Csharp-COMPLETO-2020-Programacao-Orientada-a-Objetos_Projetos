91e395aee9abbbda66e2b4dfe47afe6cb459fe44
Jose Malcher Jr <contato@josemalcher.net>
2022-05-04 14:13:31 -0300

    78 Exercício de fixação (listas)

---
+++ 6-Comportamento-de-memoria-arrays-listas/SolutionSecao6Revisao/exerLista1/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace exerLista1;
+
+public class Employee
+{
+   public int Id { get; set; }
+    public string Name { get; set; }
+    public double Salary { get; private set; }
+
+    public Employee(int id, string name, double salary)
+    {
+        Id = id;
+        Name = name;
+        Salary = salary;
+    }
+
+    public void IncreaseSalary(double percentage)
+    {
+        Salary += Salary * percentage / 100.0;
+    }
+
+    public override string ToString()
+    {
+        return Id
+               + ", "
+               + Name
+               + ", "
+               + Salary.ToString("F2", CultureInfo.InvariantCulture);
+    }
+    
+}
