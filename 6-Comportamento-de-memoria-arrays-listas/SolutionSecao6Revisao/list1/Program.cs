84982b64fa6ac2f8363bee0e59a237771c855b7d
Jose Malcher Jr <contato@josemalcher.net>
2022-05-04 09:45:41 -0300

    77 Listas (List) - PARTE 2

---
+++ 6-Comportamento-de-memoria-arrays-listas/SolutionSecao6Revisao/list1/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<string> list = new List<string>();
+list.Add("Maria");
+list.Add("Alex");
+list.Add("Bob");
+list.Add("Anna");
+list.Insert(2, "Marco");
+foreach (string obj in list)
+{
+    Console.WriteLine(obj);
+}
+Console.WriteLine("List count: " + list.Count);
+string s1 = list.Find(x => x[0] == 'A');
+Console.WriteLine("First 'A': " + s1);
+string s2 = list.FindLast(x => x[0] == 'A');
+Console.WriteLine("Last 'A': " + s2);
+int pos1 = list.FindIndex(x => x[0] == 'A');
+Console.WriteLine("First position 'A': " + pos1);
+int pos2 = list.FindLastIndex(x => x[0] == 'A');
+Console.WriteLine("Last position 'A': " + pos2);
+List<string> list2 = list.FindAll(x => x.Length == 5);
+Console.WriteLine("---------------------");
+foreach (string obj in list2)
+{
+    Console.WriteLine(obj);
+}
+list.Remove("Alex");
+Console.WriteLine("---------------------");
+foreach (string obj in list)
+{
+    Console.WriteLine(obj);
+}
+list.RemoveAll(x => x[0] == 'M');
+Console.WriteLine("---------------------");
+foreach (string obj in list)
+{
+    Console.WriteLine(obj);
+}
