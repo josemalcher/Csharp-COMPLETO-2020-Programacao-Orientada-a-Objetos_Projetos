c47fa3202a5892fb5a5e7a1d7f3a33594ae41217
Jose Malcher Jr <contato@josemalcher.net>
2022-06-07 15:36:41 -0300

    79 Matrizes

---
+++ 6-Comportamento-de-memoria-arrays-listas/SolutionSecao6Revisao/matrizes1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double[,] mat = new double[2, 3];
+Console.WriteLine(mat.Length);
+Console.WriteLine(mat.Rank);
+Console.WriteLine(mat.GetLength(0));
+Console.WriteLine(mat.GetLength(1));
